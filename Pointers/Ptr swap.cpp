f1fd7f0a307d5f6123a03b0ec58000f960b79955
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-14 12:12:00 +0530

    Update Ptr swap.cpp

--- Pointers/Ptr swap.cpp
+++ Pointers/Ptr swap.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void swap(int *p1, int *p2)//pass by pointer
+// Function to swap the values of two integers using pointers
+void swap(int *p1, int *p2) // pass by pointer
 {
-    int temp = *p1;
-    *p1 = *p2;
-    *p2 = temp;
+    int temp = *p1; // Store the value pointed by p1 in a temporary variable
+    *p1 = *p2;      // Assign the value pointed by p2 to the location pointed by p1
+    *p2 = temp;     // Assign the temporary value to the location pointed by p2
 }
 
 int main()
 {
+    int a = 10, b = 20; // Initialize two integers a and b
 
-    int a = 10, b = 20;
-
+    // Call the swap function by passing the addresses of a and b
     swap(&a, &b);
+
+    // Print the swapped values of a and b
     cout << "a = " << a << endl; // a = 20
     cout << "b = " << b << endl; // b = 10
-    return 0;
+
+    return 0; // Indicate that the program ended successfully
 }
