a900066b298505052837ea3f6cec9c52cf1c03d2
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-04-14 12:12:29 +0530

    Update Pass by reference.cpp

--- Pointers/Pass by reference.cpp
+++ Pointers/Pass by reference.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void swap(int &p1, int &p2) // pass by pointer
+// Function to swap the values of two integers using pass by reference
+void swap(int &p1, int &p2) // pass by reference
 {
-    int temp = p1;
-    p1 = p2;
-    p2 = temp;
+    int temp = p1; // Store the value of p1 in a temporary variable
+    p1 = p2;       // Assign the value of p2 to p1
+    p2 = temp;     // Assign the temporary value to p2
 }
 
 int main()
 {
+    int a = 10, b = 20; // Initialize two integers a and b
 
-    int a = 10, b = 20;
-
+    // Call the swap function by passing the variables directly
     swap(a, b);
+
+    // Print the swapped values of a and b
     cout << "a = " << a << endl; // a = 20
     cout << "b = " << b << endl; // b = 10
-    return 0;
+
+    return 0; // Indicate that the program ended successfully
 }
