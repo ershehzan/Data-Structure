675b839fe18fd2779dbcd1e8e5dac0c4fa32c711
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-05-03 20:05:21 +0530

    Update Sliding Window.cpp

--- String/Sliding Window.cpp
+++ String/Sliding Window.cpp
@@ -1,74 +1,79 @@
-//GFG
-#include <bits/stdc++.h>
+// GFG (Geeks for Geeks)
+#include <bits/stdc++.h> // Include all standard libraries for simplicity
 using namespace std;
 
-// } Driver Code Ends
-
+// Solution class containing the logic for finding the smallest substring
 class Solution {
     public:
+        // Function to find the length of the smallest substring containing all unique characters of the input string
         int findSubString(string& s) 
         {
-                vector<int> count(256, 0);
-                
-                int f = 0, sec = 0, len = s.size();
-                int dif = 0;
+            // Vector to store the frequency of each character (ASCII range 0-255)
+            vector<int> count(256, 0);
 
-                // Count number of unique characters in the string
-                while (f < s.size())
+            // Pointers and variables for the sliding window
+            int f = 0, sec = 0, len = s.size(); // f: start of window, sec: end of window, len: minimum length
+            int dif = 0; // Total number of unique characters in the string
+
+            // Step 1: Count the number of unique characters in the input string
+            while (f < s.size())
+            {
+                if (count[s[f]] == 0) // If the character is encountered for the first time
+                    dif++; // Increment the count of unique characters
+                count[s[f]]++;
+                f++;
+            }
+
+            // Step 2: Reset the count array and pointers for the sliding window
+            fill(count.begin(), count.end(), 0); // Reset all character counts to 0
+            f = 0; // Reset the start pointer
+            sec = 0; // Reset the end pointer
+            int currDif = 0; // Current number of unique characters in the window
+
+            // Step 3: Use a sliding window to find the smallest substring
+            while (sec < s.size())
+            {
+                // Expand the window until all unique characters are included
+                while (currDif < dif && sec < s.size())
                 {
-                        if (count[s[f]] == 0)
-                                dif++;
-                        count[s[f]]++;
-                        f++;
+                    if (count[s[sec]] == 0) // If a new unique character is added to the window
+                        currDif++; // Increment the count of unique characters in the window
+
+                    count[s[sec]]++; // Increment the frequency of the character
+                    sec++; // Move the end pointer
                 }
 
-                // Reset count array and pointers for sliding window
-                fill(count.begin(), count.end(), 0);
-                f = 0;
-                sec = 0;
-                int currDif = 0;
-
-                while (sec < s.size())
+                // Shrink the window while it still contains all unique characters
+                while (currDif == dif)
                 {
-                        while (currDif < dif && sec < s.size())
-                        {
-                                if (count[s[sec]] == 0)
-                                        currDif++;
+                    // Update the minimum length of the substring
+                    len = min(len, sec - f);
 
-                                count[s[sec]]++;
-                                sec++;
-                        }
+                    count[s[f]]--; // Decrement the frequency of the character at the start of the window
 
-                        while (currDif == dif)
-                        {
-                                len = min(len, sec - f);
-                                count[s[f]]--;
+                    if (count[s[f]] == 0) // If a unique character is removed from the window
+                        currDif--; // Decrement the count of unique characters in the window
 
-                                if (count[s[f]] == 0)
-                                        currDif--;
-
-                                f++;
-                        }
+                    f++; // Move the start pointer
                 }
+            }
 
-                return len;
+            // Return the length of the smallest substring
+            return len;
         }
 };
 
-//{ Driver Code Starts.
-//   Driver code
+// Driver code to test the solution
 int main() {
-        int t;
-        cin >> t;
-        while (t--) {
+    int t; // Number of test cases
+    cin >> t; // Read the number of test cases
+    while (t--) {
+        string str; // Input string
+        cin >> str; // Read the string
+        Solution ob; // Create an object of the Solution class
+        cout << ob.findSubString(str) << endl; // Print the length of the smallest substring
 
-                string str;
-                cin >> str;
-                Solution ob;
-                cout << ob.findSubString(str) << endl;
-
-                cout << "~"
-                         << "\n";
-        }
-        return 0;
+        cout << "~" << "\n"; // Separator for output
+    }
+    return 0; // End of the program
 }
