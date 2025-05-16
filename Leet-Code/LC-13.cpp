b9ffb6f6f585355d4c113c0dbf6e3eb4824d3381
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-05-16 21:31:59 +0530

    Update LC-13.cpp

--- Leet-Code/LC-13.cpp
+++ Leet-Code/LC-13.cpp
@@ -2,8 +2,10 @@
 #include <string>
 using namespace std;
 
+// Solution class to convert Roman numerals to integers
 class Solution {
 public:
+    // Helper function to return the integer value of a single Roman numeral character
     int nums(char c) {
         if (c == 'I')
             return 1;
@@ -18,20 +20,26 @@ public:
         else if (c == 'D')
             return 500;
         else
-            return 1000;
+            return 1000; // for 'M'
     }
 
+    // Function to convert a Roman numeral string to an integer
     int romanToInt(string s) {
         int sum = 0, index = 0;
 
+        // Traverse the string except the last character
         while (index < s.size() - 1) {
+            // If the current character's value is less than the next character's value,
+            // subtract it (handles cases like IV = 4, IX = 9, etc.)
             if (nums(s[index]) < nums(s[index + 1]))
                 sum -= nums(s[index]);
             else
+                // Otherwise, add its value to the sum
                 sum += nums(s[index]);
 
             index++;
         }
+        // Add the value of the last character
         sum += nums(s[index]);
         return sum;
     }
@@ -41,9 +49,11 @@ int main() {
     Solution solution;
     string romanNumeral;
 
+    // Prompt user for input
     cout << "Enter a Roman numeral: ";
     cin >> romanNumeral;
 
+    // Convert the Roman numeral to an integer and display the result
     int result = solution.romanToInt(romanNumeral);
     cout << "The integer value is: " << result << endl;
 
