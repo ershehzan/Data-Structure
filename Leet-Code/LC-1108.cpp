be1872f99b68cbe6423aee3f8412dc179435e154
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-05-16 21:30:51 +0530

    Update LC-1108.cpp

--- Leet-Code/LC-1108.cpp
+++ Leet-Code/LC-1108.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
 using namespace std;
 
+// Solution class to provide the defanging functionality
 class Solution
 {
 public:
+    // Function to defang an IP address by replacing '.' with '[.]'
     string lc(string address)
     {
-
-        // start here
+        // Initialize an empty string to build the answer
         string ans;
         size_t index = 0;
 
+        // Iterate through each character in the input string
         while (index < address.size())
         {
             if (address[index] == '.')
             {
+                // If the character is '.', append "[.]" to the answer
                 ans += "[.]";
             }
             else
             {
+                // Otherwise, append the character as-is
                 ans += address[index];
             }
-            index++;
+            index++; // Move to the next character
         }
+        // Return the defanged IP address
         return ans;
-        // end here
     }
 };
 
@@ -33,11 +37,11 @@ int main()
     Solution solution;
     string ipAddress;
 
-    // Taking user input
+    // Taking user input for the IP address
     cout << "Enter an IP address: ";
     cin >> ipAddress;
 
-    // Calling the function and displaying the result
+    // Call the defang function and display the result
     string lc = solution.lc(ipAddress);
     cout << "Defanged IP Address: " << lc << endl;
 
