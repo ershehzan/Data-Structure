ba862b95d23906b254230d43354fb61a8ff2d4f4
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-05-17 13:18:09 +0530

    Update Doubly Linked List.cpp

--- Linked-List/Doubly Linked List.cpp
+++ Linked-List/Doubly Linked List.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
+// Definition of the node class for a doubly linked list
 class node
 {
 public:
-    int data;
-    node *next;
-    node *prev;
+    int data;       // Data part of the node
+    node *next;     // Pointer to the next node
+    node *prev;     // Pointer to the previous node
+
+    // Constructor to initialize the node with a value
     node(int val)
     {
         data = val;
@@ -17,38 +20,49 @@ public:
 
 int main()
 {
-    node *head = NULL;
-    node*tail=NULL;
+    node *head = NULL; // Points to the first node of the list
+    node *tail = NULL; // Points to the last node of the list
+
     int n;
     cout << "Enter the number of Nodes: ";
     cin >> n;
-    cout << "Enter the values: ";
 
+    cout << "Enter the values: ";
     for (int i = 0; i < n; i++)
     {
         int val;
-        cin >> val;
+        cin >> val; // Read value to be inserted
 
+        // If the list is empty, create the first node and assign both head and tail
         if (head == NULL)
         {
             head = new node(val);
-            tail=head;
+            tail = head;
         }
         else
         {
-            node*temp=new node(val);
-            tail->next=temp;
-            temp->prev=tail;
-            tail=temp;
-         
+            // Create a new node
+            node *temp = new node(val);
+
+            // Link current tail to the new node
+            tail->next = temp;
+
+            // Link new node back to current tail
+            temp->prev = tail;
+
+            // Move tail pointer to the newly created node
+            tail = temp;
         }
     }
 
+    // Traverse and display the linked list
     node *curr = head;
     cout << "Doubly Linked List: ";
     while (curr != NULL)
     {
-        cout << curr->data << " ";
-        curr = curr->next;
+        cout << curr->data << " "; // Print current node's data
+        curr = curr->next;         // Move to the next node
     }
+
+    return 0;
 }
