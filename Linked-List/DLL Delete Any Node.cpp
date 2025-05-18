ad288f84201b86a2ebe3656dffcd9bde02fc2339
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-05-18 12:15:44 +0530

    Update DLL Delete Any Node.cpp

--- Linked-List/DLL Delete Any Node.cpp
+++ Linked-List/DLL Delete Any Node.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 using namespace std;
+
+// Definition of the doubly linked list node
 class node
 {
 public:
-    int data;
-    node *next;
-    node *prev;
+    int data;       // Stores the value of the node
+    node *next;     // Pointer to the next node in the list
+    node *prev;     // Pointer to the previous node in the list
+
+    // Constructor to initialize node with a value
     node(int val)
     {
         data = val;
@@ -15,87 +19,92 @@ public:
 
 int main()
 {
-
-    node *head = NULL;
-    node *tail = NULL;
+    node *head = NULL; // Pointer to the head (first node) of the DLL
+    node *tail = NULL; // Pointer to the tail (last node) of the DLL
 
     int n;
     cout << "Enter the number of nodes you want to create: ";
     cin >> n;
     cout << "Enter the values of the nodes: ";
+    // Creating the doubly linked list with n nodes
     for (int i = 0; i < n; i++)
     {
         int val;
         cin >> val;
         if (head == NULL)
         {
+            // First node creation
             head = new node(val);
             tail = head;
         }
         else
         {
+            // Appending new node at the end
             node *temp = new node(val);
-            tail->next = temp;
-            temp->prev = tail;
-            tail = temp;
+            tail->next = temp;   // Link the current tail to new node
+            temp->prev = tail;   // Link the new node back to the tail
+            tail = temp;         // Move the tail pointer to the new node
         }
     }
+
     int pos;
     cout << "Enter the position of the node you want to delete: ";
     cin >> pos;
 
+    // Deleting the node at the given position
     if (pos == 1)
     {
+        // Deleting the head node
         if (head->next == NULL)
         {
+            // Only one node in the list
             delete head;
             head = NULL;
         }
-        // if there are more than one nodes
         else
         {
+            // More than one node; update head and delete old head
             node *temp = head;
             head = head->next;
             delete temp;
-            head->prev = NULL;
+            head->prev = NULL; // Set new head's prev to NULL
         }
     }
-
     else
     {
+        // Deleting node at position other than head
         node *curr = head;
+        // Traverse to the node to be deleted
         while (--pos)
         {
             curr = curr->next;
         }
-        // last node
         if (curr->next == NULL)
         {
-            curr->prev->next = NULL;
+            // Deleting the last node (tail)
+            curr->prev->next = NULL; // Set previous node's next to NULL
             delete curr;
         }
-        // Middle node
-
         else
         {
-            node *back = curr->prev;
-            node *front = curr->next;
+            // Deleting a node from the middle
+            node *back = curr->prev;    // Node before the one to delete
+            node *front = curr->next;   // Node after the one to delete
 
-            back->next = front;
-            front->prev = back;
-            delete curr;
+            back->next = front;         // Link previous node to next node
+            front->prev = back;         // Link next node back to previous node
+            delete curr;                // Delete the target node
 
-
-            /*  Optimal solution
+            /*  Optimal solution (alternative one-liners)
             curr->prev->next = curr->next;
             curr->next->prev = curr->prev;
-            delete curr;   */ 
-            
+            delete curr;
+            */
         }
     }
 
+    // Print the list after deletion
     node *trav = head;
-
     cout << "Linked List after Deletion : ";
     while (trav)
     {
