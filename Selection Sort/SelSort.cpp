ff441b12d69472fc7ec7c9a0809a4d7e5533f9ad
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-05-26 18:40:23 +0530

    Update SelSort.cpp

--- Selection Sort/SelSort.cpp
+++ Selection Sort/SelSort.cpp
@@ -1,32 +1,41 @@
+// Selection Sort implementation in C++
+// This program sorts an array using the Selection Sort algorithm.
 
 #include <iostream>
 using namespace std;
+
 int main()
 {
-
-    int arr[1000];
+    int arr[1000]; // Array to store elements (max size 1000)
     int n;
+
+    // Prompt user for the number of elements
     cout << "Enter the Index: ";
     cin >> n;
+
+    // Prompt user to input the array elements
     cout << "Enter the Elements of Array: ";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
+    // Selection Sort algorithm
     for (int i = 0; i < n - 1; i++)
     {
-
-        int index = i;
+        int index = i; // Assume the current index is the minimum
         for (int j = i + 1; j < n; j++)
         {
+            // Find the smallest element in the unsorted part
             if (arr[j] < arr[index])
                 index = j;
         }
+        // Swap the found minimum with the first unsorted element
         swap(arr[i], arr[index]);
     }
 
-    cout<<"Sorted Array are: ";
+    // Output the sorted array
+    cout << "Sorted Array are: ";
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
