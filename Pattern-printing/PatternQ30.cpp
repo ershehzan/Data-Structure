0d4829df23adaa514d00bbce7d7775111e7c33d6
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-05-28 15:32:51 +0530

    Update PatternQ30.cpp

--- Pattern-printing/PatternQ30.cpp
+++ Pattern-printing/PatternQ30.cpp
@@ -1,58 +1,61 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // Include the input/output stream library for using cout and cin
+using namespace std; // Use the standard namespace
+
 int main()
 {
-    // i=row   j=column
-    int n, i, j;
-    cout << "Enter the number : ";
-    cin >> n;
-    // loop 1 to n
+    // i = row index, j = column index
+    int n, i, j; // Declare variables: n for size of the pattern, i/j as loop counters
+
+    cout << "Enter the number : "; // Prompt the user to enter the size of the pattern
+    cin >> n; // Read and store the user's input in n
+
+    // First half: from 1 to n rows
     for (i = 1; i <= n; i++)
     {
-        // print *
+        // Print stars on the left side
         for (j = 1; j <= i; j++)
             cout << "* ";
 
-        // print space
+        // Print spaces in the middle
         for (j = 1; j <= 2 * n - 2 * i; j++)
             cout << "  ";
 
-        // print *
+        // Print stars on the right side
         for (j = 1; j <= i; j++)
             cout << "* ";
 
-        cout << endl;
+        cout << endl; // Move to the next line after each row is printed
     }
-    // loop n-1 to 1
+
+    // Second half: from n-1 down to 1
     for (i = n - 1; i >= 1; i--)
     {
-        // print *
+        // Print stars on the left side
         for (j = 1; j <= i; j++)
             cout << "* ";
 
-        // print space
+        // Print spaces in the middle
         for (j = 1; j <= 2 * n - 2 * i; j++)
             cout << "  ";
 
-        // print *
+        // Print stars on the right side
         for (j = 1; j <= i; j++)
             cout << "* ";
 
-        cout << endl;
+        cout << endl; // Move to the next line after each row is printed
     }
 
-    return 0;
+    return 0; // Indicate successful program execution
 }
-/* Output
 
+/* Output Example for n = 5:
 *                 * 
 * *             * * 
 * * *         * * * 
 * * * *     * * * * 
 * * * * * * * * * * 
 * * * *     * * * * 
-* * *         * * * 
+* * * *         * * 
 * *             * * 
 *                 *  
-
-   */
+*/
