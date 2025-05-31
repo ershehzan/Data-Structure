84a98253340ea5b81411f799de3c0a027c95e4a0
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-05-31 11:47:39 +0530

    Update PatternQ12.cpp

--- Pattern-printing/PatternQ12.cpp
+++ Pattern-printing/PatternQ12.cpp
@@ -1,25 +1,30 @@
-#include <iostream>
+#include <iostream>  // Include the input-output stream library for cin and cout
 using namespace std;
+
 int main()
 {
-    int n, i, j;
-    cout << "Enter the number : ";
-    cin >> n;
+    int n, i, j; // n will store the number of rows, i and j are loop counters
 
+    cout << "Enter the number : "; // Prompt the user to enter the number of rows
+    cin >> n;                      // Read the value of n from user input
+
+    // Outer loop: controls the number of rows (from 1 to n)
     for (i = 1; i <= n; i++)
     {
-// Loop run from row to 1  
+        // Inner loop: prints numbers in decreasing order from the current row number (i) down to 1
         for (j = i; j >= 1; j--)
         {
-            cout << j << " ";
+            cout << j << " "; // Print the current value of j followed by a space
         }
-        cout << endl;
+        cout << endl; // Move to the next line after printing each row
     }
-    return 0;
+    return 0; // Indicate successful completion of the program
 }
-/* Output
+
+/* Output Example for n = 5:
 1 
 2 1 
 3 2 1 
 4 3 2 1 
-5 4 3 2 1    */
+5 4 3 2 1 
+*/
