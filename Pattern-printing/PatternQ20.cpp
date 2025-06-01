1fd35a9e670e00321081c1d14b1c39824f5cee8c
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-01 17:51:30 +0530

    Update PatternQ20.cpp

--- Pattern-printing/PatternQ20.cpp
+++ Pattern-printing/PatternQ20.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int n, i, j;
-    cout << "Enter the number : ";
-    cin >> n;
 
+    // Prompt the user to enter the number of rows for the pattern
+    cout << "Enter the number : ";
+    cin >> n; // Read the value of n from user input
+
+    // Loop for each row from 1 to n
     for (i = 1; i <= n; i++)
     {
-    //space print
-        for (j = 1; j <= n-i; j++)
+        // First inner loop: print spaces for right alignment
+        // For row i, print (n - i) sets of two spaces to align the letters
+        for (j = 1; j <= n - i; j++)
         {
-            cout << "  ";
+            cout << "  "; // Two spaces for each slot to match the output format
         }
-        char name ='A'+(i-1);//if we take j instead of i then o/p will be opposite
+
+        // Calculate the character to print ('A' for first row, 'B' for second, etc.)
+        char name = 'A' + (i - 1);
+        // If you use 'j' instead of 'i' here, output will be reversed
+
+        // Second inner loop: print the character for the current row, i times
         for (j = 1; j <= i; j++)
         {
             cout << name << " ";
         }
+
+        // Move to the next line after completing the current row
         cout << endl;
     }
-    return 0;
+
+    return 0; // Indicate successful program execution
 }
-/* Output
+
+/* Output Example for n = 5
 
         A 
       B B 
     C C C 
   D D D D 
 E E E E E
-   */
+
+*/
