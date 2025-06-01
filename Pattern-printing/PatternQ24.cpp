7035789536db693b82f752e794de5c072f80261c
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-01 17:53:07 +0530

    Update PatternQ24.cpp

--- Pattern-printing/PatternQ24.cpp
+++ Pattern-printing/PatternQ24.cpp
@@ -1,33 +1,41 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    // i=row   j= column
+    // i = row index, j = column index
     int n, i, j;
-    cout << "Enter the number : ";
-    cin >> n;
 
+    // Prompt the user to enter the number of rows for the pattern
+    cout << "Enter the number : ";
+    cin >> n; // Read the value of n from user input
+
+    // Loop for each row from 1 to n
     for (i = 1; i <= n; i++)
     {
-        // space print
+        // Print spaces for right alignment
+        // For row i, print (n - i) sets of two spaces
         for (j = 1; j <= n - i; j++)
-
             cout << "  ";
-        // number print
+
+        // Print the row number, i times, each followed by a space
         for (j = 1; j <= i; j++)
         {
-
             cout << i << " ";
         }
+
+        // Move to the next line after completing the current row
         cout << endl;
     }
-    return 0;
+    return 0; // Indicate successful program execution
 }
-/* Output
+
+/* Output Example for n = 5
 
         1 
       2 2 
     3 3 3 
   4 4 4 4 
 5 5 5 5 5 
-   */
+
+*/
