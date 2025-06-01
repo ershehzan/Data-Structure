5cb467f93ac0f3bfab097a84021a113607dfa192
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-01 17:52:44 +0530

    Update PatternQ23.cpp

--- Pattern-printing/PatternQ23.cpp
+++ Pattern-printing/PatternQ23.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    // i=row   j= column
+    // i = row index, j = column index
     int n, i, j;
-    cout << "Enter the number : ";
-    cin >> n;
 
+    // Prompt the user to enter the number of rows for the pattern
+    cout << "Enter the number : ";
+    cin >> n; // Read the value of n from user input
+
+    // Loop for each row from 1 to n
     for (i = 1; i <= n; i++)
     {
-        // space print
+        // Print spaces for right alignment
+        // For row i, print (n - i) spaces (single space, not double)
         for (j = 1; j <= n - i; j++)
-
-            cout << " " ;
-        // star print
+            cout << " ";
+        // Print stars for the current row, each followed by a space
         for (j = 1; j <= i; j++)
         {
-
-            cout << "* " ;
+            cout << "* ";
         }
+        // Move to the next line after completing the current row
         cout << endl;
     }
-    return 0;
+    return 0; // Indicate successful program execution
 }
-/* Output
+
+/* Output Example for n = 5
 
     * 
    * * 
@@ -31,4 +36,4 @@ int main()
  * * * * 
 * * * * * 
 
-   */
+*/
