46a28922fe7b8be84496e7d15cfbcc09ce029e30
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-10 19:43:02 +0530

    Update Subsequence.cpp

--- Recursion/Subsequence.cpp
+++ Recursion/Subsequence.cpp
@@ -4,39 +4,47 @@
 #include <vector>
 using namespace std;
 
-void subseq(int arr[], int index, int n, vector<vector<int>> &ans, vector<int>&temp)
+// Recursive function to generate all subsequences (the power set) of the input array
+// arr: input array
+// index: current position in the array
+// n: length of the array
+// ans: vector to store all generated subsequences
+// temp: current subsequence being built
+void subseq(int arr[], int index, int n, vector<vector<int>> &ans, vector<int>& temp)
 {
-
+    // Base case: if index reaches the end of the array, store the current subsequence
     if (index == n)
     {
         ans.push_back(temp);
         return;
     }
 
+    // Exclude the current element and move to the next index
     subseq(arr, index + 1, n, ans, temp);
+
+    // Include the current element in the subsequence and move to the next index
     temp.push_back(arr[index]);
     subseq(arr, index + 1, n, ans, temp);
-    temp.pop_back();
+    temp.pop_back(); // Backtrack to remove the last added element
 }
 
 int main()
 {
+    int arr[] = {1, 2, 3};                 // Example input array
+    int n = 3;                             // Number of elements in the array
+    vector<vector<int>> ans;                // Stores all subsequences (the power set)
+    vector<int> temp;                       // Temporary vector to build subsequences
 
-    int arr[] = {1, 2, 3};
-    int n = 3;
-    vector<vector<int>> ans;
-    vector<int> temp;
+    subseq(arr, 0, n, ans, temp);           // Generate all subsequences
 
-    subseq(arr, 0, n, ans, temp);
     cout << "The power set is: " << endl;
-
+    // Print all subsequences
     for (size_t i = 0; i < ans.size(); i++)
     {
+        for (size_t j = 0; j < ans[i].size(); j++)
         {
-            for (size_t j = 0; j < ans[i].size(); j++)
-            {
-                cout <<"{"<< ans[i][j]<<"}"<<","<<"";
-            }
+            cout << "{" << ans[i][j] << "}, ";
         }
+        cout << endl; // Optional: print each subsequence on a new line
     }
 }
