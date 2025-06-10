f083b1c5e58fd4757d5da3c9a834661054058148
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-10 19:41:37 +0530

    Update Prime.cpp

--- Recursion/Prime.cpp
+++ Recursion/Prime.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 using namespace std;
 
+// Recursive function to check if a number is prime
+// n: the number to check for primality
+// i: the current divisor being tested (starts from 2)
 bool isPrimeRecursive(int n, int i = 2)
 {
     if (n <= 1)
-        return false;
-    if (i == n)
-        return true;
-    if (n % i == 0)
-        return false;
+        return false;                   // Numbers less than or equal to 1 are not prime
 
+    if (i == n)
+        return true;                    // No divisors found; n is prime
+
+    if (n % i == 0)
+        return false;                   // n is divisible by i; not prime
+
+    // Recursively check for next possible divisor
     return isPrimeRecursive(n, i + 1);
 }
 
@@ -19,6 +25,7 @@ int main()
     cout << "Enter a number: ";
     cin >> n;
 
+    // Check if n is prime using the recursive function and print result
     if (isPrimeRecursive(n))
         cout << n << " is a prime number." << endl;
     else
