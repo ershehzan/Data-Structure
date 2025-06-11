5301c9cd0b256ed431227a84795676ec08391915
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-11 09:48:31 +0530

    Update Circular Linked List.cpp

--- Linked-List/Circular Linked List.cpp
+++ Linked-List/Circular Linked List.cpp
@@ -1,47 +1,63 @@
 #include <iostream>
 using namespace std;
 
+// Definition of a node in the circular linked list
 class node
 {
 public:
-    int data;
-    node *next;
+    int data;      // Value stored in the node
+    node *next;    // Pointer to the next node in the list
+
+    // Constructor to initialize a node with a value
     node(int val)
     {
         data = val;
         next = NULL;
     }
 };
-int main(){
-    node*head=NULL;
-    node*tail=NULL;
-    int n;
-    cout<<"Enter the number of nodes: ";
-    cin>>n;
 
-cout<<"Enter the values of the nodes: ";
-    for(int i=0;i<n;i++){
+int main() {
+    node *head = NULL; // Pointer to the first node (head) of the list
+    node *tail = NULL; // Pointer to the last node (tail) of the list
+    int n;             // Number of nodes to be inserted
+
+    // Prompt the user to enter the number of nodes
+    cout << "Enter the number of nodes: ";
+    cin >> n;
+
+    // Prompt the user to enter the values of the nodes
+    cout << "Enter the values of the nodes: ";
+    for (int i = 0; i < n; i++) {
         int val;
-        cin>>val;
+        cin >> val; // Read the value for the new node
 
-        if(head==NULL){
-            head=new node(val);
-            tail=head;
+        // If the list is empty, create the first node
+        if (head == NULL) {
+            head = new node(val); // The new node is both head and tail
+            tail = head;
         }
-        else{
-            node*temp=new node(val);
-            tail->next=temp;
-            tail=temp;
+        else {
+            // For subsequent nodes, create a new node and link it at the end
+            node *temp = new node(val); // Create a new node with the given value
+            tail->next = temp;          // Link the current tail to the new node
+            tail = temp;                // Move the tail pointer to the new node
         }
     }
 
-    tail->next=head;
-    cout<<"The circular linked list is: ";
-    node*curr=head;
-    while(curr){
-        cout<<curr->data<<" ";
-        curr=curr->next;
-        if(curr==head){
+    // Make the linked list circular by connecting the last node to the head
+    tail->next = head;
+
+    // Print the circular linked list
+    cout << "The circular linked list is: ";
+    node *curr = head;
+
+    // Traverse the list and print each node's data until we return to the head
+    while (curr) {
+        cout << curr->data << " "; // Print current node's data
+        curr = curr->next;         // Move to the next node
+
+        // If we've come back to the head, the traversal is complete (prevents infinite loop)
+        if (curr == head) {
             break;
         }
     }
