2176edd21dd3bc4876b7e2e427420da79aa823bf
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-17 19:22:48 +0530

    Update Merge two sorted Lists.cpp

--- Linked-List/Merge two sorted Lists.cpp
+++ Linked-List/Merge two sorted Lists.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// Definition for singly-linked list node
 class Node
 {
 public:
-    int data;
-    Node *next;
+    int data;        // Value stored in the node
+    Node *next;      // Pointer to the next node
+
     Node(int value)
     {
         data = value;
@@ -13,31 +15,35 @@ public:
     }
 };
 
+// Solution class containing merge logic
 class Solution
 {
 public:
+    // Function to merge two sorted linked lists
     Node *sortedMerge(Node *head1, Node *head2)
     {
-        Node *head = new Node(0);
-        Node *tail = head;
+        Node *head = new Node(0); // Dummy node to simplify logic
+        Node *tail = head;        // Tail pointer for merged list
 
+        // Merge nodes by comparing values
         while (head1 && head2)
         {
             if (head1->data <= head2->data)
             {
-                tail->next = head1;
-                head1 = head1->next;
-                tail = tail->next;
-                tail->next = NULL;
+                tail->next = head1;     // Attach head1 node
+                head1 = head1->next;    // Move head1 forward
+                tail = tail->next;      // Move tail forward
+                tail->next = NULL;      // Detach from old links
             }
             else
             {
-                tail->next = head2;
-                head2 = head2->next;
-                tail = tail->next;
-                tail->next = NULL;
+                tail->next = head2;     // Attach head2 node
+                head2 = head2->next;    // Move head2 forward
+                tail = tail->next;      // Move tail forward
+                tail->next = NULL;      // Detach from old links
             }
         }
+        // Attach any remaining nodes
         if (head1)
         {
             tail->next = head1;
@@ -46,14 +52,16 @@ public:
         {
             tail->next = head2;
         }
-        Node *temp = head;
-        head = head->next;
-        delete temp;
 
-        return head;
+        Node *temp = head;
+        head = head->next;   // Skip dummy node
+        delete temp;         // Free dummy node memory
+
+        return head;         // Return head of merged list
     }
 };
 
+// Function to print the linked list
 void printList(Node *head)
 {
     while (head)
@@ -64,6 +72,7 @@ void printList(Node *head)
     cout << endl;
 }
 
+// Function to create a linked list from user input
 Node *createList(int n)
 {
     Node *head = NULL;
@@ -101,6 +110,7 @@ int main()
     cout << "Enter elements of second sorted list: ";
     Node *head2 = createList(n2);
 
+    // Merge the lists
     Node *mergedHead = solution.sortedMerge(head1, head2);
 
     cout << "Merged sorted list: ";
