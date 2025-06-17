f7bcd21039331f03e0d21fd9cc5e8d68d88527be
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-17 19:24:42 +0530

    Update Insert at begin in LL.cpp

--- Linked-List/Insert at begin in LL.cpp
+++ Linked-List/Insert at begin in LL.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 
+// Definition for singly-linked list node
 class Node
 {
-
 public:
-    int data;
-    Node *next;
+    int data;      // Value stored in the node
+    Node *next;    // Pointer to the next node
 
+    // Constructor to initialize node with a value
     Node(int value)
     {
         data = value;
@@ -17,34 +18,33 @@ public:
 
 int main()
 {
-
     Node *head;
     head = NULL;
 
-    int arr[] = {20, 30, 40, 50};
+    int arr[] = {20, 30, 40, 50}; // Array of values to insert
 
-    for(int i=0;i<4;i++){
-    if (head == NULL)
-    {
-        head = new Node(arr[i]);
+    // Insert each element at the beginning of the linked list
+    for(int i = 0; i < 4; i++) {
+        if (head == NULL)
+        {
+            head = new Node(arr[i]); // First node becomes head
+        }
+        else
+        {
+            Node *temp;
+            temp = new Node(arr[i]); // Create new node
+            temp->next = head;       // Point new node to current head
+            head = temp;             // Update head to new node
+        }
     }
-    else
-    {
 
-        Node *temp;
-        temp = new Node(arr[i]);
-        temp->next = head;
-        head = temp;
-    }
-}
-cout<<"Linked List: ";
+    cout << "Linked List: ";
     // Print the linked list
-Node *current =head;
-
-while(current !=NULL){
-    cout<<current->data<<" ";
-    current=current->next;
-};
-    cout<<endl;
+    Node *current = head;
+    while(current != NULL){
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
     return 0;
 }
