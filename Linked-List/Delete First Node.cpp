ea0f19956813a9f9a7a8beaf6f4db889b2e692ba
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-18 19:58:32 +0530

    Update Delete First Node.cpp

--- Linked-List/Delete First Node.cpp
+++ Linked-List/Delete First Node.cpp
@@ -1,58 +1,62 @@
 #include <iostream>
 using namespace std;
 
+// Definition of the Node class for the singly linked list
 class Node
 {
-
 public:
-    int data;
-    Node *next;
+    int data;      // Data stored in the node
+    Node *next;    // Pointer to the next node in the list
 
+    // Constructor to initialize a node with a value
     Node(int value)
     {
         data = value;
-        next = NULL;
+        next = NULL; // By default, next is set to NULL
     }
 };
 
 int main()
 {
+    Node *head, *tail; // Pointers to the head and tail of the list
+    tail = head = NULL; // Initially, the list is empty
 
-    Node *head, *tail;
-    tail = head = NULL;
+    int arr[] = {10, 20, 30, 40, 50}; // Array of values to insert into the list
 
-    int arr[] = {10,20, 30, 40, 50};
+    // Creating the linked list from the array
     for (int i = 0; i < 5; i++)
     {
-
         if (head == NULL)
         {
-            
+            // For the first element, create a new node and set both head and tail to it
             head = new Node(arr[i]);
             tail = head;
         }
         else
         {
+            // For subsequent elements, create a new node at the end and update the tail
             tail->next = new Node(arr[i]);
             tail = tail->next;
         }
     }
+
+    // Deleting the first node of the linked list
     if (head != NULL)
     {
-        Node *temp = head;
-        head = head->next;
-        delete temp;
+        Node *temp = head;     // temp points to the node to be deleted (the first node)
+        head = head->next;     // Move head to the next node
+        delete temp;           // Delete the original first node
     }
 
     cout << "Linked List After First Node Deletion: ";
-    // Print the linked list
-    Node *current = head;
 
+    // Print the linked list after deletion
+    Node *current = head; // Pointer to traverse the list
     while (current != NULL)
     {
-        cout << current->data << " ";
-        current = current->next;
-    };
+        cout << current->data << " "; // Print data of each node
+        current = current->next;      // Move to the next node
+    }
     cout << endl;
     return 0;
 }
