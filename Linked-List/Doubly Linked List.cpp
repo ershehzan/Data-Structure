015cd31a7808e06a13e57805a914d285df63f993
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-18 19:59:22 +0530

    Update Doubly Linked List.cpp

--- Linked-List/Doubly Linked List.cpp
+++ Linked-List/Doubly Linked List.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 using namespace std;
 
+// Definition of the node class for a doubly linked list
 class node
 {
 public:
-    int data;
-    node *next;
-    node *prev;
+    int data;      // Data stored in the node
+    node *next;    // Pointer to the next node in the list
+    node *prev;    // Pointer to the previous node in the list
+
+    // Constructor to initialize a node with a value
     node(int val)
     {
         data = val;
-        next = NULL;
-        prev = NULL;
+        next = NULL; // By default, next is set to NULL
+        prev = NULL; // By default, prev is set to NULL
     }
 };
 
 int main()
 {
-    node *head = NULL;
-    node*tail=NULL;
+    node *head = NULL; // Pointer to the head (first node) of the list
+    node *tail = NULL; // Pointer to the tail (last node) of the list
     int n;
     cout << "Enter the number of Nodes: ";
     cin >> n;
     cout << "Enter the values: ";
 
+    // Creating the doubly linked list from user input
     for (int i = 0; i < n; i++)
     {
         int val;
@@ -31,24 +35,26 @@ int main()
 
         if (head == NULL)
         {
+            // For the first element, create a new node and set both head and tail to it
             head = new node(val);
-            tail=head;
+            tail = head;
         }
         else
         {
-            node*temp=new node(val);
-            tail->next=temp;
-            temp->prev=tail;
-            tail=temp;
-         
+            // For subsequent elements, create a new node at the end
+            node *temp = new node(val);
+            tail->next = temp;     // Link the current tail's next to new node
+            temp->prev = tail;     // Link new node's prev to current tail
+            tail = temp;           // Update tail to the new node
         }
     }
 
-    node *curr = head;
+    // Print the doubly linked list
+    node *curr = head; // Pointer to traverse the list
     cout << "Doubly Linked List: ";
     while (curr != NULL)
     {
-        cout << curr->data << " ";
-        curr = curr->next;
+        cout << curr->data << " "; // Print data of each node
+        curr = curr->next;         // Move to the next node
     }
 }
