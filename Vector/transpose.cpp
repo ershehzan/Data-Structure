f1e8c9f1f483827ebc09f20ca54f8b901f8d6cdd
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-20 11:40:55 +0530

    Update transpose.cpp

--- Vector/transpose.cpp
+++ Vector/transpose.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main() {
+    // Initialize a 4x4 matrix with values from 0 to 15
     vector<vector<int>> arr = {
         {0, 1, 2, 3},
         {4, 5, 6, 7},
@@ -10,7 +11,8 @@ int main() {
         {12, 13, 14, 15}
     };
 
-    // Transpose the matrix
+    // Transpose the matrix in-place
+    // Swap elements arr[i][j] with arr[j][i] for all i < j
     for (int i = 0; i < 4; i++) {
         for (int j = i + 1; j < 4; j++) {
             swap(arr[i][j], arr[j][i]);
