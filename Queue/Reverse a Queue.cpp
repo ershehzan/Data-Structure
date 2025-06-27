de4cd95c16eb826b9e5b604acf3f017912e17d86
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-27 10:47:37 +0530

    Update Reverse a Queue.cpp

--- Queue/Reverse a Queue.cpp
+++ Queue/Reverse a Queue.cpp
@@ -3,42 +3,52 @@
 #include <stack>
 using namespace std;
 
-class Solution {
-  public:
-    queue<int> reverseQueue(queue<int> &q) {
-        stack<int> st;
-        while (!q.empty()) {
-            st.push(q.front());
-            q.pop();
-        }
-        while (!st.empty()) {
-            q.push(st.top());
-            st.pop();
-        }
-        return q;
+// Function to reverse the elements of a queue using a stack
+void reverseQueue(queue<int> &q) {
+    stack<int> s; // Stack to help reverse the queue
+
+    // Step 1: Dequeue all elements from the queue and push them onto the stack
+    while (!q.empty()) {
+        s.push(q.front()); // Push the front element of the queue onto the stack
+        q.pop();           // Remove the front element from the queue
     }
-};
+
+    // Step 2: Pop all elements from the stack and enqueue them back into the queue
+    while (!s.empty()) {
+        q.push(s.top());  // Push the top element of the stack into the queue
+        s.pop();          // Remove the top element from the stack
+    }
+    // Now the queue is reversed
+}
 
 int main() {
-    Solution solution;
-    int n;
-    cout << "Enter the number of elements in the queue: ";
-    cin >> n;
     queue<int> q;
-    cout << "Enter the elements: ";
-    for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        q.push(x);
+
+    // Insert elements into the queue
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+
+    cout << "Original Queue: ";
+    // Print elements before reversing
+    queue<int> temp = q; // Make a copy to print without modifying the original
+    while (!temp.empty()) {
+        cout << temp.front() << " ";
+        temp.pop();
     }
+    cout << endl;
 
-    q = solution.reverseQueue(q);
+    // Reverse the queue
+    reverseQueue(q);
 
-    cout << "Reversed queue: ";
+    cout << "Reversed Queue: ";
+    // Print elements after reversing
     while (!q.empty()) {
         cout << q.front() << " ";
         q.pop();
     }
     cout << endl;
+
     return 0;
 }
