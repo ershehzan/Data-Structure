2433c9b3618cb9eff48268331a61c39f2fd6ddf1
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-29 15:12:26 +0530

    Update Post order iteratively.cpp

--- Tree/Post order iteratively.cpp
+++ Tree/Post order iteratively.cpp
@@ -7,35 +7,42 @@ using namespace std;
 // Definition for a binary tree node.
 struct Node
 {
-    int data;
-    Node *left;
-    Node *right;
-    Node(int val) : data(val), left(NULL), right(NULL) {}
+    int data;           // Value stored in the node
+    Node *left;         // Pointer to the left child
+    Node *right;        // Pointer to the right child
+    Node(int val) : data(val), left(NULL), right(NULL) {} // Constructor
 };
 
 class Solution
 {
 public:
+    // Function to return the postorder traversal of the binary tree iteratively
     vector<int> postOrder(Node *root)
     {
-        stack<Node *> s;
-        vector<int> ans;
+        stack<Node *> s;     // Stack to help with traversal
+        vector<int> ans;     // Vector to store postorder traversal result
+
+        // If the tree is empty, return an empty vector
         if (root == NULL)
             return ans;
-        s.push(root);
+
+        s.push(root);        // Start traversal from the root node
 
         while (!s.empty())
         {
-            Node *temp = s.top();
-            s.pop();
-            ans.push_back(temp->data);
+            Node *temp = s.top(); // Get the node on top of the stack
+            s.pop();              // Remove it from the stack
+            ans.push_back(temp->data); // Add its value to the result
 
+            // Push the left child first so that the right child is processed first (since stack is LIFO)
             if (temp->left)
                 s.push(temp->left);
 
+            // Push the right child
             if (temp->right)
                 s.push(temp->right);
         }
+        // The above process gives us a reversed postorder (root-right-left), so we reverse it
         reverse(ans.begin(), ans.end());
         return ans;
     }
@@ -44,11 +51,13 @@ public:
 // Helper function to create a simple binary tree for demonstration
 Node *buildSampleTree()
 {
-    /*   1
-        / \
-       2   3
-      / \
-     4   5  */
+    /*   Constructs the following tree:
+            1
+           / \
+          2   3
+         / \
+        4   5
+    */
     Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
@@ -59,11 +68,12 @@ Node *buildSampleTree()
 
 int main()
 {
-    Node *root = buildSampleTree();
+    Node *root = buildSampleTree(); // Build a sample tree
     Solution sol;
-    vector<int> result = sol.postOrder(root);
+    vector<int> result = sol.postOrder(root); // Get postorder traversal
 
     cout << "Postorder Traversal: ";
+    // Print the result
     for (int val : result)
     {
         cout << val << " ";
