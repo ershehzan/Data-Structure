dc6f8f7db2f48fc834e036d417f6083e2aacb696
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-06-29 15:21:50 +0530

    Update Reverse String.cpp

--- Stack/Reverse String.cpp
+++ Stack/Reverse String.cpp
@@ -3,42 +3,52 @@
 #include <stack>
 using namespace std;
 
+// Solution class containing the function to reverse a string using a stack
 class Solution {
 public:
+    // Function to reverse a vector of characters in-place using a stack
     void reverseString(vector<char>& s) {
-        stack<char> st;
+        stack<char> st; // Stack to hold characters temporarily
+
+        // Step 1: Push all characters of the vector onto the stack
         for (size_t i = 0; i < s.size(); i++) {
-            st.push(s[i]);
+            st.push(s[i]); // Push each character; last character will be on top
         }
-        int i = 0;
+
+        int i = 0; // Index for placing reversed characters back into vector
+
+        // Step 2: Pop characters from the stack and update the vector
         while (!st.empty()) {
-            s[i] = st.top();
-            st.pop();
-            i++;
+            s[i] = st.top(); // Place the top character from the stack into the vector
+            st.pop();        // Remove the character from the stack
+            i++;             // Move to next index in the vector
         }
     }
 };
 
 int main() {
     Solution solution;
-    int n;
+    int n; // Number of characters in the string
+
     cout << "Enter the number of characters: ";
-    cin >> n;
-    vector<char> s(n);
+    cin >> n; // Read the number of characters
+
+    vector<char> s(n); // Vector to hold the characters of the string
+
     cout << "Enter the characters (no spaces): ";
     for (int i = 0; i < n; i++) {
-        cin >> s[i];
+        cin >> s[i]; // Input each character individually
     }
 
+    // Call the function to reverse the string
     solution.reverseString(s);
 
+    // Output the reversed string
     cout << "Reversed string: ";
     for (char c : s) {
-        cout << c;
+        cout << c; // Print each character of the reversed string
     }
     cout << endl;
 
-
-
-    return 0;
+    return 0; // Indicate successful program termination
 }
