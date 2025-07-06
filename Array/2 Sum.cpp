1cd19bfe5aa9317f7d30a44c5a6040dd0bd551ef
Anas Khan <anas30074.@gmail.com>
2025-07-06 18:46:54 +0530

    Resolved all merge conflicts

--- Array/2 Sum.cpp
+++ Array/2 Sum.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -36,35 +35,3 @@ int main() {
     cout << "No pairs found." << endl;
     return 0;
 }
-=======
-#include <iostream>
-using namespace std;
-
-int main() {
-    int arr[1000] = {3, 4, 6, 8, 11, 12, 34, 41}; // Array to store elements
-    int n = 8, target;
-
-    cout << "Enter the target: ";
-    cin >> target;
-
-    int start = 0, end = n - 1;
-
-    // Two-pointer approach to find pairs that sum up to the target
-    while (start < end) {
-        int sum = arr[start] + arr[end];
-
-        if (sum == target) {
-            cout << "Pair found: [" << arr[start] << "," << arr[end] << "]" << endl;
-            cout << "Pair found at Index: [" << start << "," << end << "]" << endl;
-           return 0; 
-        } else if (sum > target) {
-            end--;
-        } else {
-            start++;
-        }
-    }
-
-    cout << "No pairs found." << endl;
-    return 0;
-}
->>>>>>> 549b0d8 (file update)
--- Array/2D Array.cpp
+++ Array/2D Array.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -37,40 +36,3 @@ int main()
 
     return 0; // End of the program
 }
-=======
-#include <iostream>
-using namespace std;
-
-void print(int arr[][4], int row, int col)
-{
-    for (int i = 0; i < row; i++)
-    {
-
-        for (int j = 0; j < col; j++)
-
-            cout << arr[i][j] << " ";
-    }
-
-};
-
-int main()
-{
-
-    int arr[3][4] = {1, 2, 3, 4,
-                    5, 6, 7, 8, 9, 
-                    10, 11, 12};
-
-    // i=row j=column
-    for (int i = 0; i < 3; i++)
-    {
-
-        for (int j = 0; j < 4; j++)
-        
-            cout << arr[i][j] << " ";
-        
-    }
-    print(arr, 3, 4);
-
-    return 0;
-}
->>>>>>> 549b0d8 (file update)
