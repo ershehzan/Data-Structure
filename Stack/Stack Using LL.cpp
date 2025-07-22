943bb65848b4231c842f2ed92ea882679281a4cc
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-07-22 11:21:28 +0530

    Update Stack Using LL.cpp

--- Stack/Stack Using LL.cpp
+++ Stack/Stack Using LL.cpp
@@ -1,95 +1,103 @@
 #include <iostream>
 using namespace std;
-class Node
-{
+
+// Node structure for linked list
+class Node {
 public:
     int data;
-    Node *next;
-    Node(int value)
-    {
+    Node* next;
+
+    Node(int value) {
         data = value;
         next = NULL;
     }
 };
-class Stack
-{
-    Node *top;
-    int size;
+
+// Stack using Linked List
+class Stack {
+    Node* top;     // Pointer to top node
+    int size;      // Stack size
 
 public:
-    Stack()
-    {
+    Stack() {
         top = NULL;
         size = 0;
     }
 
-    // push operation
-    void push(int value)
-    {
-        Node *temp = new Node(value);
-        if (temp == NULL)
-        {
-            cout << "Stack Overflow" << endl;
+    // Push operation
+    void push(int value) {
+        Node* temp = new Node(value);
+        if (temp == NULL) {
+            cout << "Stack Overflow (Memory Allocation Failed)" << endl;
             return;
         }
-        else
-        {
-            temp->next = top;
-            top = temp;
-            size++;
-            cout << "Pushed " << value << " to Stack" << endl;
-        }
-    };
-    // pop operation
-    void pop()
-    {
-        if (top == NULL)
-        {
+        temp->next = top;
+        top = temp;
+        size++;
+        cout << "Pushed " << value << " to Stack" << endl;
+    }
+
+    // Pop operation
+    void pop() {
+        if (top == NULL) {
             cout << "Stack Underflow" << endl;
             return;
         }
-        else
-        {
-            Node *temp = top;
-            cout << "Popped " << temp->data << " from Stack" << endl;
-            top = top->next;
-            delete temp;
-            size--;
-        }
-    };
-    //  peek operation
-    int peek()
-    {
-        if (top == NULL)
-        {
+        Node* temp = top;
+        cout << "Popped " << temp->data << " from Stack" << endl;
+        top = top->next;
+        delete temp;
+        size--;
+    }
+
+    // Peek operation
+    int peek() {
+        if (top == NULL) {
             cout << "Stack is empty" << endl;
             return -1;
         }
-        else
-        {
-            cout << "Peeked " << top->data << " from Stack" << endl;
-            return top->data;
-     
-        }
-    };
-    bool isEmpty()
-    {
-        return top == NULL;
-    };
+        cout << "Peeked " << top->data << " from Stack" << endl;
+        return top->data;
+    }
 
-    int isSize()
-    {
+    // Check if stack is empty
+    bool isEmpty() {
+        return top == NULL;
+    }
+
+    // Get current size
+    int isSize() {
         return size;
-    };
+    }
+
+    // Destructor to free all nodes
+    ~Stack() {
+        while (top) {
+            pop();  // This will delete each node
+        }
+    }
 };
-int main()
-{
+
+// Driver Code
+int main() {
     Stack s;
+
     s.push(10);
     s.push(20);
     s.push(30);
-    
-    cout<<s.peek();
 
-  
+    s.peek(); // Will also print the top
+
+    cout << "Current Stack Size: " << s.isSize() << endl;
+
+    s.pop();
+    s.pop();
+    s.pop();
+    s.pop();  // Will trigger underflow
+
+    if (s.isEmpty()) {
+        cout << "Stack is now empty." << endl;
+    }
+
+    return 0;
 }
