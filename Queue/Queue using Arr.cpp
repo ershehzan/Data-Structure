c992c6a064a0bd862edb9eae07f9aa7fec2c58ef
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-07-23 10:17:41 +0530

    Update Queue using Arr.cpp

--- Queue/Queue using Arr.cpp
+++ Queue/Queue using Arr.cpp
@@ -1,100 +1,113 @@
 #include <iostream>
 using namespace std;
 
-class Queue
-{
+class Queue {
     int front, rear;
     int *arr;
     int size;
 
 public:
-    Queue(int n)
-    {
-        arr = new int[n];
+    // Constructor
+    Queue(int n) {
+        arr = new int[n]; // dynamically allocate memory
         size = n;
-        front = rear = -1;
+        front = rear = -1; // initially queue is empty
     }
-    bool isEmpty()
-    {
+
+    // Destructor to deallocate memory
+    ~Queue() {
+        delete[] arr;
+    }
+
+    // Check if queue is empty
+    bool isEmpty() {
         return (front == -1 && rear == -1);
     }
-    bool isFull()
-    {
+
+    // Check if queue is full
+    bool isFull() {
         return (rear == size - 1);
     }
 
-    void enqueue(int x)
-    {
-        if (isEmpty())
-        {
-            cout << "Inserting " << x<<" in Queue" << endl;
+    // Enqueue operation
+    void enqueue(int x) {
+        if (isFull()) {
+            cout << "Queue is Full\n";
+            return;
+        } else if (isEmpty()) {
             front = rear = 0;
-            arr[0] = x;
-        }
-        else if (isFull())
-        {
-            cout << "Queue is Full" << endl;
-            return;
-        }
-        else
-        {
-            cout << "Inserting " << x<<" in Queue" << endl;
+        } else {
             rear++;
-            arr[rear] = x;
         }
+
+        arr[rear] = x;
+        cout << "Inserting " << x << " in Queue\n";
     }
-    void dequeue()
-    {
-        if (isEmpty())
-        {
-            cout << "Queue is Empty" << endl;
+
+    // Dequeue operation
+    void dequeue() {
+        if (isEmpty()) {
+            cout << "Queue is Empty\n";
             return;
         }
-        else
-        {
-            if (front == rear)
-            {
-                cout << "Removing " << arr[front] <<" from Queue" << endl;
-                cout << "Queue is now Empty" << endl;
-                front = rear = -1;
-            }
-            else
-            {
-                cout << "Removing " << arr[front] <<" from Queue" << endl;
-                front++;
-            }
+
+        cout << "Removing " << arr[front] << " from Queue\n";
+
+        if (front == rear) {
+            // Only one element was present
+            front = rear = -1;
+            cout << "Queue is now Empty\n";
+        } else {
+            front++;
         }
     }
 
-    int start(){
-        if(isEmpty()){
-            cout << "Queue is Empty" << endl;
+    // Return front element
+    int start() {
+        if (isEmpty()) {
+            cout << "Queue is Empty\n";
             return -1;
         }
-        else{
-            cout << "Front element is: " << arr[front] << endl;
-            return arr[front];
+
+        cout << "Front element is: " << arr[front] << endl;
+        return arr[front];
+    }
+
+    // Display current queue
+    void display() {
+        if (isEmpty()) {
+            cout << "Queue is Empty\n";
+            return;
         }
+
+        cout << "Queue elements: ";
+        for (int i = front; i <= rear; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
     }
 };
-int main(){
-   
-     int n;
+
+// Driver code
+int main() {
+    int n;
     cout << "Enter the size of the Queue: ";
     cin >> n;
+
     Queue q(n);
-    
-     cout << "Enter element to enqueue: ";
-    for(int i=0;i<n;i++){
+
+    cout << "Enter elements to enqueue:\n";
+    for (int i = 0; i < n; i++) {
         int x;
-       
         cin >> x;
         q.enqueue(x);
     }
 
-  cout << q.start() << endl;
+    q.start();      // Peek front
+    q.display();    // Display queue
+    q.dequeue();    // Remove one
+    q.start();      // Peek again
+    q.display();    // Display updated queue
 
-
-
-
+    return 0;
 }
