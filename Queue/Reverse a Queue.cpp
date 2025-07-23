af357a5335835b6a375d33a1a0ae5847ff077536
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-07-23 10:18:08 +0530

    Update Reverse a Queue.cpp

--- Queue/Reverse a Queue.cpp
+++ Queue/Reverse a Queue.cpp
@@ -4,17 +4,23 @@
 using namespace std;
 
 class Solution {
-  public:
+public:
+    // Function to reverse the queue using a stack
     queue<int> reverseQueue(queue<int> &q) {
         stack<int> st;
+
+        // Step 1: Transfer all queue elements to the stack
         while (!q.empty()) {
             st.push(q.front());
             q.pop();
         }
+
+        // Step 2: Transfer back from stack to queue (reversed)
         while (!st.empty()) {
             q.push(st.top());
             st.pop();
         }
+
         return q;
     }
 };
@@ -22,8 +28,10 @@ class Solution {
 int main() {
     Solution solution;
     int n;
+    
     cout << "Enter the number of elements in the queue: ";
     cin >> n;
+
     queue<int> q;
     cout << "Enter the elements: ";
     for (int i = 0; i < n; i++) {
@@ -32,13 +40,16 @@ int main() {
         q.push(x);
     }
 
+    // Reverse the queue
     q = solution.reverseQueue(q);
 
+    // Print reversed queue
     cout << "Reversed queue: ";
     while (!q.empty()) {
         cout << q.front() << " ";
         q.pop();
     }
+
     cout << endl;
     return 0;
 }
