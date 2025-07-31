5b89bdb6a05997fd795740cc5d729da3c0b0d85f
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-07-31 10:04:36 +0530

    Update Recursive Deletion Approach.cpp

--- Linked-List/Recursive Deletion Approach.cpp
+++ Linked-List/Recursive Deletion Approach.cpp
@@ -1,11 +1,15 @@
-
+// Include necessary header file for input/output operations
 #include <iostream>
 using namespace std;
+
+// Definition of the node class for linked list
 class node
 {
 public:
-    int data;
-    node *next;
+    int data;      // Value stored in the node
+    node *next;    // Pointer to the next node
+
+    // Constructor to initialize node with a value
     node(int value)
     {
         data = value;
@@ -13,56 +17,67 @@ public:
     }
 };
 
-node *deletenode(node*current,int x){
-    if(x==1){
-        node*temp=current->next;
-        delete current;
-        return temp;
-
+// Recursive function to delete the node at position 'x'
+node *deletenode(node* current, int x) {
+    // Base case: if x is 1, we need to delete the current node
+    if (x == 1) {
+        node* temp = current->next; // Store next node
+        delete current;             // Delete current node
+        return temp;                // Return new head of the list
     }
-    current->next=deletenode(current->next,x-1);
-    return current;
+    // Recursive case: move to the next node, decrement position
+    current->next = deletenode(current->next, x - 1);
+    return current; // Return current node as part of the updated list
 }
-
 
 int main()
 {
+    node *head = NULL; // Pointer to head of linked list
+    node *tail = NULL; // Pointer to tail of linked list
 
-    node *head = NULL;
-    node *tail = NULL;
+    int arr[100];      // Array to store node values
+    int n;             // Number of nodes
 
-    int arr[100];
-    int n;
+    // Get number of nodes from user
     cout << "Enter the number of Nodes for linked list: ";
     cin >> n;
+
+    // Get node values from user
     cout << "Enter the elements of Nodes in linked list: ";
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
+    // Create linked list from input values
     for (int i = 0; i < n; i++)
     {
         if (head == NULL)
         {
+            // If list is empty, create the first node
             head = new node(arr[i]);
             tail = head;
         }
         else
         {
+            // Otherwise, append new node to the end
             tail->next = new node(arr[i]);
             tail = tail->next;
         }
     }
 
-    int x;
+    int x; // Position of node to delete
+
+    // Get position from user for deletion
     cout << "Enter the value of the node to be deleted: ";
     cin >> x;
 
-    head=deletenode(head,x);
+    // Delete the node at position x and update head
+    head = deletenode(head, x);
+
+    // Print the linked list after deletion
     cout << "Linked List After Deletion: ";
     node *curr = head;
-    
     while (curr)
     {
         cout << curr->data << " ";
