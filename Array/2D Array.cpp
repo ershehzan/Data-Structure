50ab758e6a19ace440b680e5a323342116289ce0
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-08-09 09:29:23 +0530

    Update 2D Array.cpp

--- Array/2D Array.cpp
+++ Array/2D Array.cpp
@@ -26,6 +26,7 @@ int main()
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 4; j++)
+            
             cout << arr[i][j] << " "; // Print current element followed by a space
     }
 
