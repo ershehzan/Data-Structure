a1f64dbb00d0659c0f05a204037b249c870d4d40
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-08-09 09:29:41 +0530

    Update 3 Sum.cpp

--- Array/3 Sum.cpp
+++ Array/3 Sum.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int main() {
+int main() 
+{
     // Initialize a vector with unsorted elements
     vector<int> arr = {1, 8, 6, 4, 10, 45};
 
