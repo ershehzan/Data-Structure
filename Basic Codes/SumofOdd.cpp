9084f094a96bb876b0641ed7286675256839f772
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-08-10 19:10:54 +0530

    Update SumofOdd.cpp

--- Basic Codes/SumofOdd.cpp
+++ Basic Codes/SumofOdd.cpp
@@ -6,9 +6,10 @@ int main()
     int n, sum = 0;
     
     cout << "Enter the number of terms: ";
+    
     cin >> n;
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 1; i <= n; i++) 
     {
         if (i % 2 != 0)
         {
