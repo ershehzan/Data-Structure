b5cf3af743ed84d64dc8a9773d2446b1f1df0f85
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-09-20 19:20:03 +0530

    Add files via upload

---
+++ Graph/Bipartite Graph.cpp
@@ -0,0 +1,78 @@
+//BFS approach to check if a graph is bipartite or not
+//Time Complexity: O(V + E
+//Space Complexity: O(V)
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isBipartite(int V, vector<vector<int>> &edges)
+    {
+        vector<int> adj[V];
+        for (auto e : edges)
+        {
+            int v = e[0], u = e[1];
+            adj[v].push_back(u);
+            adj[u].push_back(v);
+        }
+        vector<int> color(V, -1);
+        queue<int> q;
+
+        for (int i = 0; i < V; ++i)
+        {
+            if (color[i] == -1)
+            {
+                q.push(i);
+                color[i] = 0;
+                while (!q.empty())
+                {
+                    int node = q.front();
+                    q.pop();
+                    for (int j = 0; j < adj[node].size(); j++)
+                    {
+                        int neigh = adj[node][j];
+                        if (color[neigh] == -1)
+                        {
+                            color[neigh] = (color[node] + 1) % 2;
+                            q.push(neigh);
+                        }
+                        else
+                        {
+                            if (color[neigh] == color[node])
+                                return 0;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
+
+int main()
+{
+    int V, E;
+    cout << "Enter number of vertices and edges: ";
+    cin >> V >> E;
+    vector<vector<int>> edges;
+    cout << "Enter edges (u v):" << endl;
+    for (int i = 0; i < E; ++i)
+    {
+        int u, v;
+        cin >> u >> v;
+        edges.push_back({u, v});
+    }
+    Solution sol;
+    if (sol.isBipartite(V, edges))
+    {
+        cout << "Graph is Bipartite" << endl;
+    }
+    else
+    {
+        cout << "Graph is NOT Bipartite" << endl;
+    }
+    return 0;
+}
