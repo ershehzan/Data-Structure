31cc323ecbca70803787dac1621b31daecdc34dd
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-10-15 19:35:01 +0530

    Update Deletion in BST.cpp

--- Tree/Binary Search Tree ( BST )/Deletion in BST.cpp
+++ Tree/Binary Search Tree ( BST )/Deletion in BST.cpp
@@ -134,12 +134,11 @@ Node *deleteNode(Node *root, int x)
     }
 }
 
-int main()
-{
+int main(){
     int n; // Number of nodes to be inserted
 
     cout << "Enter the number of nodes in the BST: ";
     cin >> n;
 
     Node *root = NULL; //*
-
+}
