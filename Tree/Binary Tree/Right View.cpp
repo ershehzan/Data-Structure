6bb932991a51bf30853a6959153e92481ffb6b29
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-10-16 11:34:11 +0530

    Update Right View.cpp

--- Tree/Binary Tree/Right View.cpp
+++ Tree/Binary Tree/Right View.cpp
@@ -59,8 +59,7 @@ Node *buildSampleTree(){
     return root;
 }
 
-int main()
-{
+int main(){
     Node *root = buildSampleTree();
     Solution sol;
     vector<int> right_view = sol.rightView(root);
