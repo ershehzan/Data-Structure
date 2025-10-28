f39bfa098fd73ba9d31720726cd038f31e1d3e45
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-10-28 19:23:11 +0530

    Update BinarySclass.cpp

--- Binary Search/BinarySclass.cpp
+++ Binary Search/BinarySclass.cpp
@@ -2,8 +2,7 @@
 #include <algorithm> // For sorting the array
 using namespace std;
 // Function to perform binary search
-int binarySearch(int arr[], int n, int target)
-{
+int binarySearch(int arr[], int n, int target){
     int left = 0, right = n - 1, mid;
     while (left <= right) {
         mid = left + (right - left) / 2; // Avoids potential integer overflow
