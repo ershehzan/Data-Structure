0cd4c6e6f853afc1abf5527527c4cc1dcc24bf2f
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-10-30 18:07:46 +0530

    Update Floyd_Warshall.cpp

--- Graph/Floyd_Warshall.cpp
+++ Graph/Floyd_Warshall.cpp
@@ -8,8 +8,7 @@ public:
     void floydWarshall(vector<vector<int>> &dist) {
         
         int n = dist.size();
-        for (int k = 0; k < n; k++)
-        {
+        for (int k = 0; k < n; k++)  {
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
