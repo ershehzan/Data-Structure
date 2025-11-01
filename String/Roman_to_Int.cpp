cdf3f550a43c13eea4761d508f04bc999e4ad36d
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-01 19:15:34 +0530

    Update Roman_to_Int.cpp

--- String/Roman_to_Int.cpp
+++ String/Roman_to_Int.cpp
@@ -60,8 +60,7 @@ public:
     }
 };
 
-int main()
-{
+int main(){
     Solution solution;   // Create an instance of the Solution class
     string romanNumeral; // Variable to store the user-input Roman numeral
 
