40aa6f1b54dfc4f389a4da28c0897bce3816782b
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-07 09:40:36 +0530

    Update Binary Tree using Recursion.cpp

--- Tree/Binary Tree/Binary Tree using Recursion.cpp
+++ Tree/Binary Tree/Binary Tree using Recursion.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 // Definition of a Node in the binary tree
-class Node
-{
+class Node{
 public:
     int data;         // Value stored in the node
     Node *left, *right; // Pointers to the left and right child nodes
