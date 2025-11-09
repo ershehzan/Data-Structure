1b7fb4feef307f8bf3cb73cdf0da44102e1513ce
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-09 19:42:12 +0530

    Update Vertical Tree Traversal.cpp

--- Tree/Binary Tree/Vertical Tree Traversal.cpp
+++ Tree/Binary Tree/Vertical Tree Traversal.cpp
@@ -30,8 +30,7 @@ void find(Node *root, int pos, int &l, int &r){
     find(root->right, pos + 1, l, r);
 }
 
-class Solution
-{
+class Solution{
 public:
     vector<vector<int>> verticalOrder(Node *root)
     {
