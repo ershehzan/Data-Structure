4d73ad7cea405c25102d77915ce1d8fd514e1322
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-10 18:41:39 +0530

    Update BST Using Recursion.cpp

--- Tree/Binary Search Tree ( BST )/BST Using Recursion.cpp
+++ Tree/Binary Search Tree ( BST )/BST Using Recursion.cpp
@@ -9,8 +9,7 @@ public:
     Node *right;     // Pointer to the right child
 
     // Constructor to initialize the node with a value
-    Node(int value)
-    {
+    Node(int value)  {
         data = value;          // Set the node's data
         left = right = NULL;   // Initialize left and right pointers to NULL
     }
