9b86847185cc9dc33ebcd03c7148022391461b0a
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-13 18:09:04 +0530

    Update LC-153.cpp

--- Leet-Code/LC-153.cpp
+++ Leet-Code/LC-153.cpp
@@ -6,9 +6,7 @@ int main(){
     int arr[1000] = {4, 5, 6, 7, 8, 10, 1, 2};
     int start = 0, end = 7, ans = arr[0];
 
-    while (start <= end)
-    {
-
+    while (start <= end){
         int mid = start + (end - start) / 2;
         if (arr[mid] >= arr[0])
         {
@@ -23,5 +21,6 @@ int main(){
     cout << ans;
     return 0;
 }
+
 
 
