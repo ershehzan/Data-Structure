17db8eaa6d57bb1af0876d2670f456b23c74532e
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-14 19:29:51 +0530

    Update LC-206.cpp

--- Leet-Code/LC-206.cpp
+++ Leet-Code/LC-206.cpp
@@ -21,8 +21,7 @@ int main(){
     vector<int> arr = {10, 20, 30, 40, 50};
     int n = arr.size();
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
 
         if (head == NULL)
         {
