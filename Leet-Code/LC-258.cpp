00a78c552593ab08366bed5b98c2eda29dcefcad
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-16 18:02:41 +0530

    Update LC-258.cpp

--- Leet-Code/LC-258.cpp
+++ Leet-Code/LC-258.cpp
@@ -9,8 +9,7 @@ int main(){
     cin >> num;
 
     // Loop until the number reduces to a single digit
-    while (num > 9) 
-    {
+    while (num > 9)   {
         int rem, ans = 0;  // Variables to store remainder and sum of digits
         
         while (num != 0) // Extract each digit and add it
