8f3e2190d09c9e3468b720ef2cadc382d0314fc0
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-17 16:55:20 +0530

    Update LC-7.cpp

--- Leet-Code/LC-7.cpp
+++ Leet-Code/LC-7.cpp
@@ -5,10 +5,8 @@ int main(){
     int n;
     cout << "Enter a number: ";
     cin >> n;
-
     int ans = 0, rem;
-    while (n != 0)
-    {
+    while (n != 0) {
         rem = n % 10;         // Extract the last digit
         ans = ans * 10 + rem; // Shift digits in `ans` left and add the new digit
         n /= 10;              // Remove the last digit from `n`
