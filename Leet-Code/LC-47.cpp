2b4568be062bbfe63847060f72eacf89209e680b
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-17 16:53:30 +0530

    Update LC-47.cpp

--- Leet-Code/LC-47.cpp
+++ Leet-Code/LC-47.cpp
@@ -20,7 +20,7 @@ void permute(vector<int> &arr, vector<vector<int>> &ans, int index,int n){
     }
 }
 
-int main(){
+int main() {
 
     vector<int> arr = {1, 2, 2, 1};
     int n = arr.size();
@@ -37,6 +37,7 @@ int main(){
 
     return 0;
 }
+
 
 
 
