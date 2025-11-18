3e00e4f226a04093966b10ab6baa9a09d7465219
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-18 17:36:39 +0530

    Update Heap Sort.cpp

--- Heap/Heap Sort.cpp
+++ Heap/Heap Sort.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
 // Iterative version of heapify to maintain the max-heap property
 void heapify(vector<int> &arr, int i, int n){
     
