c91a1d89b1b97b963751ee380ac0644528d3d718
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-20 18:05:20 +0530

    Update Target sum Repeatition.cpp

--- Recursion/Target sum Repeatition.cpp
+++ Recursion/Target sum Repeatition.cpp
@@ -10,7 +10,6 @@ int subsum(int arr[], int index,int sum,int n){
 int main(){
     int arr[] = {2,3,4};
     int sum = 6;
-
     cout << subsum(arr, 0, sum,3); // Call the function to find the target sum
 
     return 0;
