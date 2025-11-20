c8368ddcac047de9bada08aa5cbb467c9a62baff
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-20 18:05:47 +0530

    Update Target sum Repeatition.cpp

--- Recursion/Target sum Repeatition.cpp
+++ Recursion/Target sum Repeatition.cpp
@@ -5,6 +5,7 @@ int subsum(int arr[], int index,int sum,int n){
     
     if(sum==0)
     return 1;
+    
     if(index==n|| sum<0)
     return 0;
     return subsum(arr, index+1 , sum,n) + subsum(arr, index, sum - arr[index],n);
