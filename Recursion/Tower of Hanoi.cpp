91cab13cbd1b36f7e2937b6407e8ca6446c66b57
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-20 18:01:27 +0530

    Update Tower of Hanoi.cpp

--- Recursion/Tower of Hanoi.cpp
+++ Recursion/Tower of Hanoi.cpp
@@ -2,8 +2,7 @@
 #include <iostream>
 using namespace std;
 
-void toh(int n, int sou, int help, int des)
-{
+void toh(int n, int sou, int help, int des){
     if (n == 1)
     {
         cout << "Move disk 1 from rod " << sou << " to rod " << des << endl;
