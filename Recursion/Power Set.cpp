a48c88b167d9495ac7aae9d14a172caf36d644ba
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-21 18:37:25 +0530

    Update Power Set.cpp

--- Recursion/Power Set.cpp
+++ Recursion/Power Set.cpp
@@ -8,10 +8,8 @@ void subseq(string &s, int index, int n, vector<string>& ans, string temp){
         ans.push_back(temp);
         return;
     }
-
     // Exclude the current character
     subseq(s, index + 1, n, ans, temp);
-
     // Include the current character
     temp.push_back(s[index]);
     subseq(s, index + 1, n, ans, temp);
