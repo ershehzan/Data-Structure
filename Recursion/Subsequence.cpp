6edc1ba29ac87a9b67e11bd1a525c89545f08860
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-21 18:32:38 +0530

    Update Subsequence.cpp

--- Recursion/Subsequence.cpp
+++ Recursion/Subsequence.cpp
@@ -4,8 +4,7 @@
 #include <vector>
 using namespace std;
 
-void subseq(int arr[], int index, int n, vector<vector<int>> &ans, vector<int>&temp)
-{
+void subseq(int arr[], int index, int n, vector<vector<int>> &ans, vector<int>&temp){
 
     if (index == n)
     {
