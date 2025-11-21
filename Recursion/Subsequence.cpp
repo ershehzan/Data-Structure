2065d202fd91d9ee1b9591c8ff95f99213e303e3
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-21 18:33:55 +0530

    Update Subsequence.cpp

--- Recursion/Subsequence.cpp
+++ Recursion/Subsequence.cpp
@@ -24,9 +24,7 @@ int main(){
     cout << "The power set is: " << endl;
 
     for (size_t i = 0; i < ans.size(); i++){
-        {
-            for (size_t j = 0; j < ans[i].size(); j++)
-            {
+            for (size_t j = 0; j < ans[i].size(); j++){
                 cout <<"{"<< ans[i][j]<<"}"<<","<<"";
             }
         }
