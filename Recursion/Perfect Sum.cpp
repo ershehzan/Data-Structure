de5f889a63702563c06cdc82a39ee1bddb0e7451
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-22 16:54:45 +0530

    Update Perfect Sum.cpp

--- Recursion/Perfect Sum.cpp
+++ Recursion/Perfect Sum.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 int subset(int arr[], int index,int sum,int n){
+    
     if (index == n)
         return sum == 0;
     return subset(arr, index + 1, sum,n) + subset(arr, index + 1, sum - arr[index],n);
