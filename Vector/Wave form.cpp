d2357b40e1fa83af4aee641d5ae9d6e6d2b05678
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-11-28 11:30:53 +0530

    Update Wave form.cpp

--- Vector/Wave form.cpp
+++ Vector/Wave form.cpp
@@ -5,10 +5,12 @@ using namespace std;
 int main()
 {
 
-    vector<vector<int>> matrix = {
+    vector<vector<int>> matrix = 
+    {
         {1, 2, 3, 4},
         {5, 6, 7, 8},
-        {9, 10, 11, 12}};
+        {9, 10, 11, 12}
+    };
 
     cout << "wave form" << endl;
 
