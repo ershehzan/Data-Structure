e8eea62e3b3f667b86d21e2a4094e3329834d408
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-12-06 16:05:28 +0530

    Update Course_Schedule.cpp

--- Graph/Course_Schedule.cpp
+++ Graph/Course_Schedule.cpp
@@ -8,8 +8,7 @@ public:
     vector<int> findOrder(int n, vector<vector<int>> prerequisites) {
         vector<int> Indeg(n, 0);
         vector<int> adj[n];
-        for (auto &p : prerequisites)
-        {
+        for (auto &p : prerequisites){
             int u = p[0];
             int v = p[1];
             adj[v].push_back(u);
