717212cf4d9f8d66fa6a87db02b87c4ab44f37d8
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-12-08 18:31:59 +0530

    Update Deletion at end.cpp

--- Array/Deletion at end.cpp
+++ Array/Deletion at end.cpp
@@ -6,7 +6,6 @@ int main()
     int n; // Variable to store the number of elements in the array
     cout << "Enter the number of elements in the array: ";
     cin >> n; // Read the number of elements from the user
-
     int arr[1000]; // Declare an array with a maximum size of 1000
     cout << "Enter " << n << " elements:" << endl;
     for (int i = 0; i < n; i++)
