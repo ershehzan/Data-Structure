64dfa1e8baf22ccf1ebc51e4f4c6b435d672e6e8
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-12-09 18:56:24 +0530

    Update PatternQ9.cpp

--- Pattern-printing/PatternQ9.cpp
+++ Pattern-printing/PatternQ9.cpp
@@ -6,8 +6,7 @@ int main(){
     cout << "Enter the number : ";
     cin >> n;
 
-    for (i = 1; i <= n; i++)
-    {
+    for (i = 1; i <= n; i++){
 
         for (j = 1; j <= i; j++)// Loop run from 1 to no. of rows
         {
