ede12ab253e0daaef889f53d64533241b94dfd6c
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-12-09 18:57:15 +0530

    Update PatternQ8.cpp

--- Pattern-printing/PatternQ8.cpp
+++ Pattern-printing/PatternQ8.cpp
@@ -10,8 +10,7 @@ int main(){
     cin >> n;
     
     for (i = 1; i <= n; i++){
-        for (j = 1; j <= n; j++)
-        {
+        for (j = 1; j <= n; j++){
            // cout << (i - 1) * 5 + j << " "; //Alternate solution
             cout << count << " ";
             count++;
