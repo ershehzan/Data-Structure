e0306b926c250a8c52f766524df3788e77279d28
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-12-09 18:58:47 +0530

    Update PatternQ7.cpp

--- Pattern-printing/PatternQ7.cpp
+++ Pattern-printing/PatternQ7.cpp
@@ -6,9 +6,8 @@ int main(){
     int n, i, j;
     cout << "Enter a number :";
     cin >> n;
-    for (i = 1; i <= n; i++)
-    {
-      
+    
+    for (i = 1; i <= n; i++)  {  
         // Nested loop
         for (j = 1; j <= n; j++)
         {
