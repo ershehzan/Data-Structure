fcf674e21f52b294f6eb0df86b843f0beb0e83cf
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-12-12 18:36:56 +0530

    Update 2 Sum.cpp

--- Array/2 Sum.cpp
+++ Array/2 Sum.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int main() 
-{
+int main() {
     // Initialize an array with some integers (sorted order)
     int arr[1000] = {3, 4, 6, 8, 11, 12, 34, 41}; // Array to store elements
  
@@ -40,6 +39,7 @@ int main()
  
     return 0;//return 
 }
+
 
 
 
