ab04d0d9d2f476e50f3ed11b6f2e061fbb5f956f
SHEHZAN KHAN <ershehzankhan@gmail.com>
2025-12-12 18:22:02 +0530

    Update Reverse each row in matrix.cpp

--- Array/Reverse each row in matrix.cpp
+++ Array/Reverse each row in matrix.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main(){
+    
     int arr[4][4] = {{1, 2, 3, 4},
                      {5, 6, 7, 8},
                      {9, 10, 11, 12},
@@ -30,4 +30,5 @@ int main()
     }
     return 0;
 }
+
 
