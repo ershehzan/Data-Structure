f4735dc7c5e6075217f0de06085f1e96c4565297
SHEHZAN KHAN <ershehzankhan@gmail.com>
2026-02-13 16:19:59 +0530

    list

--- STL in C++/list.cpp
+++ STL in C++/list.cpp
@@ -5,20 +5,31 @@ using namespace std;
 int main()
 {
 
-    list<int>l1;
-    l1.push_back(1);
-    l1.push_back(2);
-    l1.push_back(3);
-    l1.push_back(4);
-    l1.push_back(5);
-    l1.push_back(6);
+    list<int>l1={1,2,3,4,5,6,7,8,9,10};
+    // l1.push_back(1);
+    // l1.push_back(2);
+    // l1.push_back(3);
+    // l1.push_back(4);
+    // l1.emplace_back(5);
+    // l1.emplace_back(6);
 
     cout << "List elements: ";
     cout<<l1.front()<<" ";
     cout<<l1.back()<<" ";
-    for(auto i = l1.begin(); i != l1.end(); ++i) {
-        cout << *i << " ";
+    cout<<endl;
+    
+    for(auto i:l1)
+    {
+         cout << i << " ";
     }
+    
+    // for(auto i = l1.begin(); i != l1.end(); i++) {
+    //     cout << *i << " ";
+    // }
+    
+    //   for(list<int>::iterator i = l1.begin(); i != l1.end(); i++) {
+    //     cout << *i << " ";
+    // }
     cout << endl;
     l1.pop_back();
     l1.pop_front();
